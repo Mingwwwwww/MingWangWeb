1cf63e6c7881d63ebafca396e0539a013f1c638f
Ming Wang <99439011+Mingwwwwww@users.noreply.github.com>
2024-09-04 14:11:22 -0600

    Update stackbit.config.ts
    fix for visual editor

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,10 @@
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "hugo",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+    stackbitVersion: "~0.6.0",
+    nodeVersion: "18",
+    ssgName: "hugo",
+    contentSources: [],
+    postInstallCommand: "npm i --no-save @stackbit/types",
+    devCommand: "hugo serve -D"  // 添加这一行来指定开发服务器命令
+});
